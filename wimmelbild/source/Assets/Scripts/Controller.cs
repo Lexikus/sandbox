55d2093e8dc67b155d11c284b2bd1184b616d7cb
Alexander <alexander.bifulco@gmail.com>
2019-10-04 12:44:54 +0200

    wimmelbild

---
+++ wimmelbild/source/Assets/Scripts/Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private GameObject winCanvas;
+    private int missingElements = 10;
+
+    public static Controller Instance { get; private set; }
+
+    private void Awake() {
+        if (Instance == null) Instance = this;
+        else if (Instance != this) Destroy(this);
+    }
+
+    public void TriggerFoundItem() {
+        missingElements--;
+
+        if(missingElements < 1) {
+            ShowWinningCanvas();
+        }
+    }
+
+    public void ShowWinningCanvas() {
+        winCanvas.SetActive(true);
+    }
+
+    public void ReloadGame() {
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ wimmelbild/source/Assets/Scripts/ImageTextPair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageTextPair : MonoBehaviour {
+    [SerializeField] private Text text;
+    private GameObject parentGameObject;
+
+    private void Start() {
+        parentGameObject = transform.parent.gameObject;
+    }
+
+    public void OnClick() {
+        Controller.Instance.TriggerFoundItem();
+        parentGameObject.SetActive(false);
+        text.text = "<color=#00ff00ff>" + text.text + "</color>";
+        Destroy(this);
+    }
+}
